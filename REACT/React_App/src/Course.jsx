a4ff91e08827ac5048bc1459bed78d823b41730d
SREE-VISHNU-N <sreevishnu.n28@gmail.com>
2025-06-23 21:46:05 +0530

    Buttons(Buy Course,Delete)

--- REACT/React_App/src/Course.jsx
+++ REACT/React_App/src/Course.jsx
@@ -1,16 +1,32 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react';
 
 
 function Course(props) {
 
-   
-        return (
-            <div className="card">
-                <img src={props.image} alt="" />
-                <h2>{props.name}</h2>
-                <p>{props.price}</p>
-            </div>
-        );
+    //let Purchased = false;
+
+    const [Purchased, setParchased] = useState(false);
+
+    function BuyCourse(discount) {
+        console.log(props.name, 'Purchased with', discount, '% Discount');
+        setParchased(true);
+        console.log(Purchased);
+    }
+
+
+    return (
+        props.name && <div className="card">
+            <img src={props.image} alt="" />
+            <h2>{props.name}</h2>
+            <p>{props.price}</p>
+            <button onClick={() => BuyCourse(20)}>Buy Course</button>
+            <button onClick={() => props.delete(props.id)}>Delete</button>
+            <p>{Purchased ? 'Already Purchased' : 'Get it out'}</p>
+
+
+        </div>
+    );
 }
 
 Course.PropTypes = {
@@ -18,7 +34,5 @@ Course.PropTypes = {
     price: PropTypes.number,
     show: PropTypes.bool
 }
-
-
 
 export default Course
--- REACT/React_App/src/Courses.jsx
+++ REACT/React_App/src/Courses.jsx
@@ -2,36 +2,53 @@ import Course from './Course'
 import html from './assets/HTML.png'
 import css from './assets/CSS.png'
 import js from './assets/JS.png'
-
-
+import react from './assets/REACT.png'
+import { useState } from 'react'
 
 function courseList() {
-    const Courses = [
+    const [Courses, setCourses] = useState([
         {
             id: 1,
             name: 'HTML',
-            price: "$199",
+            price: 199,
             image: html
         },
         {
             id: 2,
             name: 'CSS',
-            price: "$199",
+            price: 199,
             image: css
         },
         {
             id: 3,
             name: 'Java Script',
-            price: "$499",
+            price: 499,
             image: js
+        },
+        {
+            id: 4,
+            name: 'REACT',
+            price: 999,
+            image: react
         }
-    ]
 
+
+    ]);
+
+    function handleDelete(id) {
+        const newCourses = Courses.filter((course) => course.id != id);
+        setCourses(newCourses);
+
+    }
+
+
+    Courses.sort((x, y) => y.price - x.price)
 
     const CoursesLists = Courses.map((course) =>
-        <Course key = {course.id}name={course.name} price={course.price} image={course.image} />
+        <Course key={course.id} name={course.name} 
+                price={course.price} image={course.image} 
+                delete={handleDelete} id={course.id} />
     );
-
 
     return (
         <> {CoursesLists}</>
