10c27c5c573eb6b52ed2946ebf1e6af31dca5dd9
SREE-VISHNU-N <sreevishnu.n28@gmail.com>
2025-06-27 21:38:41 +0530

    (Error Handling-Loding)

--- REACT/React_App/src/Courses.jsx
+++ REACT/React_App/src/Courses.jsx
@@ -4,18 +4,24 @@ import { useEffect, useState } from 'react'
 function courseList() {
     const [Courses, setCourses] = useState(null);
     //npx json-server --watch data/dummyData.json --port 3000 --static ./data
-
-
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((response)=>{
-            console.log(response);
-            return response.json();
-        })
-        .then((data)=>{
-            console.log(data);
-        })
-    },[]);
+const [error, setError] = useState(null);
+    useEffect(() => {
+        setTimeout(()=>{
+        fetch('http://localhost:3000/Courses')
+            .then((response) => {
+                if(!response.ok){
+                    throw Error("This page is Could't driven")
+                }
+                console.log(response);
+                return response.json();
+            })
+            .then((data) => {
+                setCourses(data)
+            }).catch((error)=>{
+                console.log(error.message);
+                setError(error.message);
+            })},1000)
+    }, []);
 
     function handleDelete(id) {
         const newCourses = Courses.filter((course) => course.id != id);
@@ -24,12 +30,18 @@ function courseList() {
     }
 
 
-    Courses.sort((x, y) => y.price - x.price)
-
+    // Courses.sort((x, y) => y.price - x.price)
+    if (!Courses) {
+     
+        return (<>
+    {error && <h3 className="error">{error}</h3>}
+    {!error && <div className="loding"><img src="data\assets\Loding.gif"/></div>}
+        </>)
+    }
     const CoursesLists = Courses.map((course) =>
-        <Course key={course.id} name={course.name} 
-                price={course.price} image={course.image} 
-                delete={handleDelete} id={course.id} />
+        <Course key={course.id} name={course.name}
+            price={course.price} image={course.image}
+            delete={handleDelete} id={course.id} />
     );
 
     return (
