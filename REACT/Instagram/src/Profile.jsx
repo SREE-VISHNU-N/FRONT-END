aa57156005635fa921be911a6c51ba91387efceb
SREE-VISHNU-N <sreevishnu.n28@gmail.com>
2025-08-13 21:16:18 +0530

    Work on Profile & Little modification in sidebar

--- REACT/Instagram/src/Profile.jsx
+++ REACT/Instagram/src/Profile.jsx
@@ -18,10 +18,10 @@ function Profile() {
                 <div className='d-flex justify-content-center '>
                     <div className='mt-5'><img src={Profile.profilePic} alt='Profile' className='profile rounded-circle' /></div>
                     <div className='profileContent d-flex'>
-                        <div><p className='me-3 fw-normal' style={{fontSize:'18px'}}>{Profile.username}</p></div>
-                        <div><p className=' grayBox me-3 fw-medium' style={{fontSize:'13px'}}>Edit profile</p></div>
-                            <div><p className=' grayBox me-3 fw-medium' style={{fontSize:'13px'}}>View archive</p></div>
-                        <div><i class="bi bi-gear-wide"></i></div>
+                        <div><p className='me-3 fw-normal' style={{ fontSize: '18px' }}>{Profile.username}</p></div>
+                        <div><p className=' grayBox me-3 fw-medium' style={{ fontSize: '13px' }}>Edit profile</p></div>
+                        <div><p className=' grayBox me-3 fw-medium' style={{ fontSize: '13px' }}>View archive</p></div>
+                        <div><i className="bi bi-gear-wide"></i></div>
                     </div>
                 </div>
 
@@ -29,31 +29,36 @@ function Profile() {
 
                     <div className='align-items-center'>
                         <div className='d-flex mt-3 mb-1'>
-                            <p className='fw-semibold me-1' style={{fontSize:'14px'}}>{Profile.post}</p><span className='me-4' style={{fontSize:'14px', color:"gray"}}>posts</span>
-                            <p className='fw-semibold me-1' style={{fontSize:'14px'}}>{Profile.followers}</p><span className='me-4' style={{fontSize:'14px', color:"gray"}}>followers</span>
-                            <p className='fw-semibold me-1' style={{fontSize:'14px'}}>{Profile.following}</p><span className='me-4' style={{fontSize:'14px', color:"gray"}}>following</span>
+                            <p className='fw-semibold me-1' style={{ fontSize: '14px' }}>{Profile.post}</p><span className='me-4' style={{ fontSize: '14px', color: "gray" }}>posts</span>
+                            <p className='fw-semibold me-1' style={{ fontSize: '14px' }}>{Profile.followers}</p><span className='me-4' style={{ fontSize: '14px', color: "gray" }}>followers</span>
+                            <p className='fw-semibold me-1' style={{ fontSize: '14px' }}>{Profile.following}</p><span className='me-4' style={{ fontSize: '14px', color: "gray" }}>following</span>
                         </div>
                         <div>
-                            <div><p className='m-0 fw-medium' style={{fontSize:'15px'}}>John Cena </p></div>
-                            <div><p className='m-0' style={{fontSize:'14px'}}>🏆The Last Real Champ </p></div>
-                            <div><p className='m-0' style={{fontSize:'14px'}}>🔹17-Time World Champion</p></div>
-                            <div><p className='m-0' style={{fontSize:'14px'}}>🔥Hustle. Loyalty. Respect.</p></div>
-                            <div><p className='m-0' style={{fontSize:'14px'}}>🎤WWE Icon | 🎬Hollywood Star</p></div>
-                            <div><p className='m-0' style={{fontSize:'14px'}}>👋"You Can’t See Me" - But I See Greatness</p></div>
+                            <div><p className='m-0 fw-medium' style={{ fontSize: '15px' }}>John Cena </p></div>
+                            <div><p className='m-0' style={{ fontSize: '14px', color: 'black' }}>🏆The Last Real Champ </p></div>
+                            <div><p className='m-0' style={{ fontSize: '14px', color: 'black' }}>🔹17-Time World Champion</p></div>
+                            <div><p className='m-0' style={{ fontSize: '14px', color: 'black' }}>🔥Hustle. Loyalty. Respect.</p></div>
+                            <div><p className='m-0' style={{ fontSize: '14px', color: 'black' }}>🎤WWE Icon | 🎬Hollywood Star</p></div>
+                            <div><p className='m-0' style={{ fontSize: '14px', color: 'black' }}>👋"You Can’t See Me" - But I See Greatness</p></div>
+                            <div><a className='Flink' href="https://www.linkedin.com/in/sree-vishnu-n/" target='_blank'><i class="bi bi-facebook"></i>Sree Vishnu</a></div>
                         </div>
-
                     </div>
-
-
                 </div>
 
+                <div>
+                    <div className='new'>
+                        <i className="bi bi-plus-circle-fill"></i>
+                        <p className='newLetter'>New</p>
+                </div>
+            </div>
 
             </div>
 
-            ) : (
-                <div className=' d-flex justify-content-center'><img className='spinner' src="http://localhost:5000/assets/Spinner-2.gif" alt="Loading" /></div>
-            )}
-        </div>
+    ) : (
+        <div className=' d-flex justify-content-center'><img className='spinner' src="http://localhost:5000/assets/Spinner-2.gif" alt="Loading" /></div>
+    )
+}
+        </div >
     )
 }
 
--- REACT/Instagram/src/Sidebar.jsx
+++ REACT/Instagram/src/Sidebar.jsx
@@ -3,21 +3,42 @@ import { useNavigate } from 'react-router-dom'
 
 function Sidebar() {
 
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
   return (
-    <div className='container d-flex flex-column justify-content-between fs-6 mx-2 mb-0'>
+    <div className='container d-flex flex-column justify-content-between fs-5 mx-2 mb-0'>
       <img className='logo_text' src="db\assets\Insta_text.png" />
-      <div className='fw-bold'><i className="bi bi-house-door-fill fs-5"></i>Home</div>
-      <div><i className="bi bi-search fs-5"></i>Search</div>
-      <div><i className="bi bi-compass fs-5"></i>Explore</div>
-      <div><i className="bi bi-film fs-5"></i>Reels</div>
-      <div><i className="bi bi-chat-dots fs-5"></i>Messages</div>
-      <div><i className="bi bi-heart fs-5"></i>Notifications</div>
-      <div><i className="bi bi-plus-circle fs-5"></i>Create</div>
-      <div><i className="bi bi-bar-chart-steps fs-5"></i>Dashboard</div>
-      <div onClick={()=>navigate('/Profile')}><img className='profile_picture' src="db\assets\john_cena.jpg" />Profile</div>
-      <div><i className="bi bi-list fs-5"></i>More</div>
+
+      <div className='fw-bold'><i className="bi bi-house-door-fill fs-5" style={{ color: '#000000' }}></i>
+        <span style={{ fontSize: '15px', color: '#000000' }}>Home</span></div>
+
+      <div><i className="bi bi-search fs-5" style={{ color: '#000000' }}></i>
+        <span style={{ fontSize: '15px', color: '#000000' }}>Search</span></div>
+
+      <div><i className="bi bi-compass fs-5" style={{ color: '#000000' }}></i>
+        <span style={{ fontSize: '15px', color: '#000000' }}>Explore</span></div>
+
+      <div><i className="bi bi-film fs-5" style={{ color: '#000000' }}></i>
+        <span style={{ fontSize: '15px', color: '#000000' }}>Reels</span></div>
+
+      <div><i className="bi bi-chat-dots fs-5" style={{ color: '#000000' }}></i>
+        <span style={{ fontSize: '15px', color: '#000000' }}>Messages</span></div>
+
+      <div><i className="bi bi-heart fs-5" style={{ color: '#000000' }}></i>
+        <span style={{ fontSize: '15px', color: '#000000' }}>Notifications</span></div>
+
+      <div><i className="bi bi-plus-circle fs-5" style={{ color: '#000000' }}></i>
+        <span style={{ fontSize: '15px', color: '#000000' }}>Create</span></div>
+
+      <div><i className="bi bi-bar-chart-steps fs-5" style={{ color: '#000000' }}></i>
+        <span style={{ fontSize: '15px', color: '#000000' }}>Dashboard</span></div>
+
+      <div onClick={() => navigate('/Profile')}><img className='profile_picture' src="db\assets\john_cena.jpg" />
+        <span style={{ fontSize: '15px', color: '#000000' }}>Profile</span></div>
+
+      <div><i className="bi bi-list fs-5" style={{ color: '#000000' }}></i>
+        <span style={{ fontSize: '15px', color: '#000000' }}>More</span></div>
+
     </div>
   )
 }
