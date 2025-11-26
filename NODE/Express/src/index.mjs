a5afa6c314ef31ddefad972d5a4ef3f199d8ee6a
SREE-VISHNU-N <sreevishnu.n28@gmail.com>
2025-11-26 19:11:16 +0530

    post, put, patch, delete req with custom  middleware

--- NODE/Express/src/index.mjs
+++ NODE/Express/src/index.mjs
@@ -12,17 +12,48 @@ const users = [
 ]
 
 const products = [
-    {id:1, productName: "iphone 17"},
-    {id:2, productName: "iphone 16"},
-    {id:3, productName: "s25 ultra"},
-    {id:4, productName: "oppo k13"},
-    {id:5, productName: "realme p4"}
+    { id: 1, productName: "iphone 17" },
+    { id: 2, productName: "iphone 16" },
+    { id: 3, productName: "s25 ultra" },
+    { id: 4, productName: "oppo k13" },
+    { id: 5, productName: "realme p4" }
 ]
 
 
 app.listen(PORT, () => {
     console.log(`This is running in port ${PORT}`);
 })
+
+app.use(express.json()); // middleware for convert json data from user
+
+
+const getUserIndexById = (req, res, next) => { // This is the middleware for get "id" from req, and get "userIndex"  by id.
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) {
+        res.status(404).send({ msg: 'Bad request, Invalid ID' });
+    }
+
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    if (userIndex === -1) {
+        res.status(404).send({ msg: 'User is not found' });
+    }
+
+    req.userIndex = userIndex;
+    req.id = id;
+    next();
+}
+
+const getParamsId = (req, res, next) => {
+    const id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+        res.status(404).send({ msg: "Bad Request, Invalid ID" });
+    }
+    req.id = id;
+    next();
+}
+
 
 app.get('/', (req, res) => {
     res.send({ msg: "This is the Index page" });
@@ -41,14 +72,9 @@ app.get('/api/users', (req, res) => {         //Get data by search username
 })
 
 
-app.get('/api/users/:id', (req, res) => {     //Get data by id
+app.get('/api/users/:id', getParamsId, (req, res) => {     //Get data by id
 
-    const id = parseInt(req.params.id);
-
-    if (isNaN(id)) {
-        res.status(404).send({ msg: "Bad Request, Invalid ID" });
-    }
-
+    const id = req.id;
     const user = users.find((user) => user.id === id);
 
     if (user) {
@@ -59,58 +85,69 @@ app.get('/api/users/:id', (req, res) => {     //Get data by id
 })
 
 
-app.get('/api/products',(req,res)=>{
+app.get('/api/products', (req, res) => {
 
-    const {filter, value} = req.query;
+    const { filter, value } = req.query;
 
-    if(filter && value) {
-        return res.send(products.filter(((product)=>product[filter].toLowerCase().includes(value))));
+    if (filter && value) {
+        return res.send(products.filter(((product) => product[filter].toLowerCase().includes(value))));
     }
+    res.send(products);
 })
 
 
-app.get('/api/products/:id',(req,res)=>{
-    const id = parseInt(req.params.id);
-    if(isNaN(id)){
-        res.status(404).send({msg:"Bad request, Invalid ID"});
-    }
-    
-    const product = products.find((user)=>user.id === id);
+app.get('/api/products/:id', getParamsId, (req, res) => {
+    const id = req.id;
 
-    if(product){
+    const product = products.find((user) => user.id === id);
+
+    if (product) {
         return res.send(product);
     }
 
-    return res.send({msg: "Product is not found"});
+    return res.send({ msg: "Product is not found" });
 })
 
-app.use(express.json()); // middleware for convert json data from user
 
-app.post('/api/users',(req,res)=>{  // post request method for store data from frontend
+
+app.post('/api/users', (req, res) => {  // post request method for store new data from frontend
     console.log(req.body);
-    const {body} = req;
-    const newUser = {id: users[users.length-1].id+1, ...body};
+    const { body } = req;
+    const newUser = { id: users[users.length - 1].id + 1, ...body };
     users.push(newUser);
     res.status(201).send(newUser);
 })
 
 
-app.put('/api/users/:id',(req,res)=>{
-    const id = parseInt(req.params.id);
-    if(isNaN(id)){
-        res.status(404).send({msg:'Bad request, Invalid ID'});
-    }
+app.put('/api/users/:id', getUserIndexById, (req, res) => {  // it will updated the entire data by index
+    const userIndex = req.userIndex;
+    const id = req.id;
 
-    const userIndex = users.findIndex((user)=>user.id === id);
+    const { body } = req;
 
-    if(userIndex === -1){
-        res.status(404).send({msg:'User is not found'});
-    }
-     const {body}=req;
-    users[userIndex]={id: id, ...body};
-    res.status(200).send({msg:'Data updated'})
+    users[userIndex] = { id: id, ...body };
+    res.status(200).send({ msg: 'Data updated' })
 
 })
 
 
+app.patch("/api/users/:id", getUserIndexById, (req, res) => { // it update particular element of object based on their index
+
+    const userIndex = req.userIndex;
+
+    const { body } = req;
+
+    users[userIndex] = { ...users[userIndex], ...body };
+    res.status(200).send({ msg: "The data is updated" });
+})
+
+
+app.delete("/api/users/:id", getUserIndexById, (req, res) => {  // it delete the object based on their index
+
+    const userIndex = req.userIndex;
+
+    users.splice(userIndex, 1);
+    res.status(200).send({ msg: 'Element is deleted' });
+
+})
 // query -> http://localhost:3000/users?filter=userName&value=sr
